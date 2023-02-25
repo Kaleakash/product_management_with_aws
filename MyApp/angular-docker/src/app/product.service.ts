e4769191c32be561279c396c0c0f223704e213b9
akash <akash300383@gmail.com>
2023-02-25 20:23:23 +0530

    done

---
+++ MyApp/angular-docker/src/app/product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  baseUrl:string ="http://localhost:9292/products"
+  constructor(public http:HttpClient) { }
+
+  loadAllProducts():Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl+"/findAllProducts",{responseType:"json"});
+  }
+
+  storeProduct(product:Product):Observable<string> {
+    return this.http.post(this.baseUrl+"/storeProduct",product,{responseType:"text"});
+  }
+}
